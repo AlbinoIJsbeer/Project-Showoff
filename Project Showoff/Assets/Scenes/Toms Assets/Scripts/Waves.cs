8917473e4925ce6abff81e2f77497c539618e0fa
drilonbajrami <54851175+drilonbajrami@users.noreply.github.com>
2020-05-12 22:28:29 +0200

    U

--- Project Showoff/Assets/Scenes/Toms Assets/Scripts/Waves.cs
+++ Project Showoff/Assets/Scenes/Toms Assets/Scripts/Waves.cs
@@ -69,14 +69,17 @@ public class Waves : MonoBehaviour
 
     public float GetHeight(Vector3 position)
     {
+        //scale factor and position in local space
         var scale = new Vector3(1 / transform.lossyScale.x, 0, 1 / transform.lossyScale.z);
         var localPos = Vector3.Scale((position - transform.position), scale);
 
+        //get edge points
         var p1 = new Vector3(Mathf.Floor(localPos.x), 0, Mathf.Floor(localPos.z));
         var p2 = new Vector3(Mathf.Floor(localPos.x), 0, Mathf.Ceil(localPos.z));
         var p3 = new Vector3(Mathf.Ceil(localPos.x), 0, Mathf.Floor(localPos.z));
         var p4 = new Vector3(Mathf.Ceil(localPos.x), 0, Mathf.Ceil(localPos.z));
 
+        //clamp if the position is outside the plane
         p1.x = Mathf.Clamp(p1.x, 0, Dimensions);
         p1.z = Mathf.Clamp(p1.z, 0, Dimensions);
         p2.x = Mathf.Clamp(p2.x, 0, Dimensions);
@@ -86,19 +89,23 @@ public class Waves : MonoBehaviour
         p4.x = Mathf.Clamp(p4.x, 0, Dimensions);
         p4.z = Mathf.Clamp(p4.z, 0, Dimensions);
 
+        //get the max distance to one of the edges and take that to compute max - dist
         var max = Mathf.Max(Vector3.Distance(p1, localPos), Vector3.Distance(p2, localPos), Vector3.Distance(p3, localPos), Vector3.Distance(p4, localPos) + Mathf.Epsilon);
         var dist = (max - Vector3.Distance(p1, localPos))
                  + (max - Vector3.Distance(p2, localPos))
                  + (max - Vector3.Distance(p3, localPos))
                  + (max - Vector3.Distance(p4, localPos) + Mathf.Epsilon);
-
+        //weighted sum
         var height = _mesh.vertices[index(p1.x, p1.z)].y * (max - Vector3.Distance(p1, localPos))
-                   + _mesh.vertices[index(p2.x, p2.z)].y * (max - Vector3.Distance(p1, localPos))
-                   + _mesh.vertices[index(p3.x, p3.z)].y * (max - Vector3.Distance(p1, localPos))
-                   + _mesh.vertices[index(p4.x, p4.z)].y * (max - Vector3.Distance(p1, localPos));
+                   + _mesh.vertices[index(p2.x, p2.z)].y * (max - Vector3.Distance(p2, localPos))
+                   + _mesh.vertices[index(p3.x, p3.z)].y * (max - Vector3.Distance(p3, localPos))
+                   + _mesh.vertices[index(p4.x, p4.z)].y * (max - Vector3.Distance(p4, localPos));
 
+        //scale
         return height * transform.lossyScale.y / dist;
+
     }
+
 
     private Vector3[] GenerateVertices()
     {
