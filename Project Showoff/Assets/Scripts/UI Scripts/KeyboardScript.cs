d80dd2492647591afdf7b00da7c5bb27aa538ccb
AlbinoIJsbeer <tomdekinkelder@live.nl>
2020-06-11 12:43:46 +0200

    Added Keyboard
    Added a keyboard. Pliz dont make me do this again XD

---
+++ Project Showoff/Assets/Scripts/UI Scripts/KeyboardScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class KeyboardScript : MonoBehaviour
+{
+
+    private string _word = null;
+    private int _wordIndex = 0;
+    private string alpha;
+    [SerializeField]
+    private TMP_InputField _name = null;
+
+    public void alphabetFunction(string _alphabet)
+    {
+        _wordIndex++;
+        _word = _word + _alphabet;
+        _name.text = _word;
+    }
+
+
+}
